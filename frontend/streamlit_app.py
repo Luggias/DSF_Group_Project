9a8b075fd8dbb54690871bd4ffaa0449ecfcd6ea
Lukas Kapferer <157932318+Luggias@users.noreply.github.com>
2025-12-10 14:52:53 +0100

    minor changes

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -37,9 +37,8 @@ def main():
         st.image(str(LOGO_PATH), width=160)
     st.title("Staffing estimation for Restaurant Sonne")
     st.write(
-        "Please select upcoming holidays in the left menu. \n"
-        "Set special events for each of the next 7 days here: "
-        ""
+        "Please select upcoming holidays in the left menu.  \n"
+        "Set special events for each of the next 7 days here:  \n"
         "[Restaurant website](https://www.sonneseehotel.ch/de)"
     )
 
