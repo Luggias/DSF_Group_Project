7b1c5d5237c205134af86c510b543c9648f8d367
Lukas Kapferer <157932318+Luggias@users.noreply.github.com>
2025-12-10 13:54:15 +0100

    change text to display

--- frontend/streamlit_app.py
+++ frontend/streamlit_app.py
@@ -37,8 +37,8 @@ def main():
         st.image(str(LOGO_PATH), width=160)
     st.title("Staffing estimation for Restaurant Sonne")
     st.write(
-        "Select upcoming holiday levels in the left menu."
-        "Set special events and holiday levels for each of the next 7 days."
+        "Please select the values for upcoming holidays in the left menu. "
+        "Set special events for each of the next 7 days here:"
     )
 
     model, feature_cols, births_map, weather_fallback, history, last_known_date = load_model()
